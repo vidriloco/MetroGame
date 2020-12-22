2d9a220bc4eb451a76d103c53cc42ffff8bdba84
Alejandro Cruz Paz <alejandrop@backbase.com>
2020-12-22 20:02:05 +0100

    First commit

---
+++ Assets/Tilemap/Scripts/MoveObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Rigidbody rb;
+    public Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.velocity = (direction * speed);
+    }
+}
---
+++ Assets/Tilemap/Scripts/Testing.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using CodeMonkey;
+
+public class Testing : MonoBehaviour {
+
+    [SerializeField] private TilemapVisual tilemapVisual;
+    private Tilemap tilemap;
+    private Tilemap.TilemapObject.TilemapSprite tilemapSprite;
+
+
+    [SerializeField]
+    private GameObject carPrefab;
+    [SerializeField]
+    private float spawnDelay = 10;
+    private float nextSpawnTime;
+
+    public float speed = 10.0f;
+    GameObject carMoving;
+    public Vector2 movement;
+
+    private void Start() {
+        tilemap = new Tilemap(20, 10, 5f, Vector3.zero);
+
+        tilemap.SetTilemapVisual(tilemapVisual);
+
+        tilemap.Load();
+    }
+
+    private void Spawn()
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        carMoving = Instantiate(carPrefab, tilemap.position() + new Vector3(20*4,22,0), Quaternion.Euler(0, 0, 0));
+        
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time > nextSpawnTime;
+    }
+
+    void FixedUpdate()
+    {
+        if(carMoving != null)
+        {
+            carMoving.GetComponent<Rigidbody>().velocity = new Vector2(-3, 0) * speed;
+        }
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        //rb.velocity = direction * speed;
+    }
+
+    private void Update() {
+        if(ShouldSpawn()) {
+            Spawn();
+        }
+
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (Input.GetMouseButton(0)) {
+            Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+            tilemap.SetTilemapSprite(mouseWorldPosition, tilemapSprite);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.T)) {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.None;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.VehicleLaneManHoleA;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.VehicleLaneA;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.VehicleLaneManHoleB;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.VehicleLaneB;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.Sidewalk;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.Path;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.Pasture;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.P)) {
+            tilemap.Save();
+            CMDebug.TextPopupMouse("Saved!");
+        }
+        if (Input.GetKeyDown(KeyCode.L)) {
+            tilemap.Load();
+            CMDebug.TextPopupMouse("Loaded!");
+        }
+
+    }
+
+}
