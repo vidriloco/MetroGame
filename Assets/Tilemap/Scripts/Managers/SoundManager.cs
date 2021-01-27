959e4d4411d284ef7329beb1853979f166d98554
Alejandro Cruz Paz <alejandrop@backbase.com>
2021-01-27 10:17:52 +0100

    refactor of sound manager

---
+++ Assets/Tilemap/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] humanReactionClips;
+    [SerializeField] private AudioClip arrivingMetro;
+    [SerializeField] private AudioClip closingDoors;
+    [SerializeField] private AudioClip departingMetro;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayRandomHumanSound()
+    {
+        var sound = humanReactionClips[UnityEngine.Random.Range(0, humanReactionClips.Length)];
+        gameObject.GetComponent<AudioSource>().PlayOneShot(sound);
+    }
+
+    public void PlayMetroSoundArriving()
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(arrivingMetro);
+    }
+
+    public void PlayMetroSoundClosingDoors()
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(closingDoors);
+    }
+
+    public void PlayMetroSoundDeparting()
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(departingMetro);
+    }
+}
